3c749dadc5f847638fdc13bb0dc33cc50b56da63
Jiyeon You <94870473+Jiyeon31@users.noreply.github.com>
2022-03-14 00:56:18 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Innisfree1201!',
+    password: '',
     database: 'employeelist'
   });
   
